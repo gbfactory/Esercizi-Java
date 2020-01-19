def0d32edeebbaa63d46cc30d1b3380cd7b63b31
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-01-19 15:08:09 +0100

    Create MainRettangolo.java

---
+++ Oggetti/MainRettangolo.java
@@ -0,0 +1,61 @@
+package oggetti;/*
+
+/*
+ * Descrizione: Main per test rettangolo
+ * Autore: Cognome nome
+ * Data: 18 01 2020
+ * Versione: 0.1
+ */
+
+public class MainRettangolo {
+
+	public static void main(String[] args) {
+		
+		// Dichiarazione e creazione rettangolo r
+		Rettangolo r = new Rettangolo();	// Richiama costruttore di default
+		
+		// Stampo base e altezza
+		System.out.println(r.getBase());	// Metodo getBase
+		System.out.println(r.getAltezza());	// Metodo getAltezza
+		
+		// Imposto base e altezza
+		r.setBase(3);						// Metodo setBase
+		r.setAltezza(5);					// Metodo setAltezza
+		
+		// Visualizzo nuovamente
+		r.visualizza();
+
+
+		// Esempi precedenti
+
+		/*Scanner tastiera=new Scanner(System.in);
+		System.out.println("Rettangolo r1 -----------------");
+		Rettangolo r1;
+		r1=new Rettangolo();
+		r1.visualizza();
+		System.out.println("L'area e': "+r1.calcoloArea());
+		System.out.println("Il perimetro e': "+r1.calcoloPerimetro());
+		System.out.println();
+		
+		System.out.println("Rettangolo r2 -----------------");
+		Rettangolo r2;
+		System.out.println("Inserisci la base:");
+		int base=tastiera.nextInt();
+		System.out.println("Inserisci l'altezza:");
+		int altezza=tastiera.nextInt();
+		r2=new Rettangolo(base,altezza);
+		
+		r2.visualizza();
+		
+		System.out.println("L'area e': "+r2.calcoloArea());
+		System.out.println("Il perimetro e': "+r2.calcoloPerimetro());
+		System.out.println();
+		
+		System.out.println("Rettangolo r3 -----------------");
+		Rettangolo r3=new Rettangolo(5, 7);
+		r3.visualizza();
+		System.out.println("L'area e': "+r3.calcoloArea());
+		System.out.println("Il perimetro e': "+r3.calcoloPerimetro());*/
+	}
+
+}
