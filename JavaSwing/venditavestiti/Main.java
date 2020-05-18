2df03fb3ad3c595842c91960e7751e1a5206cb10
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-05-18 09:03:28 +0200

    Create Main.java

---
+++ JavaSwing/venditavestiti/Main.java
@@ -0,0 +1,29 @@
+package venditaabbigliamento;
+
+/**
+ * Vendita Abbigliamento
+ *
+ * Creare una interfaccia grafica che permetta di effettuare un ordine di un capo di abbigliamento.
+ * Si deve prevedere di ...
+ * inserire Cognome, Nome di un cliente
+ * selezionare  tipo di abbigliamento (es. maglietta, felpa,...)
+ * selezionare taglia (es. S, L, XL)
+ * selezionare tipo maniche (lunghe o corte)
+ * selezionare uomo o donna
+ * selezionare con cappuccio, con cerniera, con bottoni
+ * visualizzare un prezzo che si aggiorna continuamente in base alle scelte fatte (algoritmo del calcolo del prezzo a piacere...)
+ *
+ * Class: Main
+ *
+ * @author gbfactory
+ *
+ * @since 15/05/2020
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Frame();
+    }
+
+}
